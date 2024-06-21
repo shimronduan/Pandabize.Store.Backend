735285db23af55600798d4deb85c4270fc8dee79
Shimron Duan <Shimron_Duan@next.co.uk>
2024-06-21 10:27:12 +0100

    Domain

---
+++ Pandabize.Store.Domain/Entities/Item.cs
@@ -0,0 +1,10 @@
+﻿using Pandabize.Store.Domain.Common;
+
+namespace Pandabize.Store.Domain.Entities
+{
+    public class Item : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
