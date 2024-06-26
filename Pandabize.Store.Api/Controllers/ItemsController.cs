8e992d8c351dafb2c322982657f6532df9f81080
Shimron Duan <Shimron_Duan@next.co.uk>
2024-06-26 20:44:02 +0100

    setup controller

--- Pandabize.Store.Api/Controllers/ItemsController.cs
+++ Pandabize.Store.Api/Controllers/ItemsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Pandabize.Store.Api.Controllers
@@ -7,5 +8,19 @@ namespace Pandabize.Store.Api.Controllers
     [Route("api/[controller]")]
     public class ItemsController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public ItemsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet(Name = "GetAllEvents")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<List<string>>> GetAllEvents()
+        {
+            return Ok();
+        }
     }
 }
